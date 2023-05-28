035768e74731143fcc301efaf3fc2f6a0d5aadd7
udp1024 <udp1024@hotmail.com>
2023-05-28 13:26:52 -0600

    updated to Udacity python data extraction course tutorials

---
+++ JSON/command-line-setup.py
@@ -0,0 +1,75 @@
+# exec(open("JSON/command-line-setup.py").read())
+import json
+import requests
+
+BASE_URL = "http://musicbrainz.org/ws/2/"
+ARTIST_URL = BASE_URL + "artist/"
+
+query_type = {  "simple": {},
+                "atr": {"inc": "aliases+tags+ratings"},
+                "aliases": {"inc": "aliases"},
+                "releases": {"inc": "releases"}}
+
+def query_site(url, params, uid="", fmt="json"):
+    """
+    This is the main function for making queries to the musicbrainz API. The
+    query should return a json document.
+    """
+    params["fmt"] = fmt
+    r = requests.get(url + uid, params=params)
+    print("requesting", r.url)
+    if r.status_code == requests.codes.ok:
+        return r.json()
+    else:
+        r.raise_for_status()
+
+def query_by_name(url, params, name):
+    """
+    This adds an artist name to the query parameters before making an API call
+    to the function above.
+    """
+    params["query"] = "artist:" + name
+    return query_site(url, params)
+
+def pretty_print(data, indent=4):
+    """
+    After we get our output, we can use this function to format it to be more
+    readable.
+    """
+    if type(data) == dict:
+        print(json.dumps(data, indent=indent, sort_keys=True))
+    else:
+        print(data)
+
+results = query_by_name(ARTIST_URL, query_type["simple"], "One Direction")
+
+artistCount = 0
+itemcount = len(results['artists'])
+# for i in range(itemcount):
+#     if (results['artists'][i]['name'] == "Queen") and (results['artists'][i]['type'] == 'Group'):
+#         print(i, results['artists'][i]['name'], 'id',results['artists'][i]['id'])
+#         #artistCount += 1
+#         print(i)
+
+
+i=0
+for i in range(itemcount):
+    print(results['artists'][i]['name'], results['artists'][i]['type'])
+    if (results['artists'][i]['name'] == "One Direction") and (results['artists'][i]['type'] == "Group"):
+        print(json.dumps(results["artists"][i]["life-span"]["begin"]))
+        break
+
+# numofAliases=len(results['artists'][i]['aliases'])
+# j=0
+# for j in range(numofAliases):
+#     if (results['artists'][i]['aliases'][j]['locale'] == "es"):
+#         print(i, j, "es")
+#         results['artists'][i]['aliases'][j]['name']
+#         break
+
+
+# print(artistCount)
+# print(json.dumps(results, indent=4))
+# OneDdatafile = open("OneD-data.json", "w")
+# OneDdatafile.write(json.dumps(results, indent=4))
+# OneDdatafile.close()
--- JSON/musicbrainz.py
+++ JSON/musicbrainz.py
@@ -27,7 +27,6 @@ def query_site(url, params, uid="", fmt="json"):
     params["fmt"] = fmt
     r = requests.get(url + uid, params=params)
     print("requesting", r.url)
-
     if r.status_code == requests.codes.ok:
         return r.json()
     else:
@@ -60,6 +59,12 @@ def main():
     exploration. Modify the function calls and indexing below to answer the
     questions on the next quiz.
 
+    1. How many bands named "First Aid Kit"?
+    2. Begin-Area Name for Queen?
+    3. Spanish Alias for Beatles?
+    4. Nirvana disambiguation?
+    5. When was one direction formed?
+
     HINT: Note how the output we get from the site is a multi-level JSON
     document, so try making print statements to step through the structure one
     level at a time or copy the output to a separate output file. Experimenting
@@ -67,7 +72,7 @@ def main():
     """
 
     # Query for information in the database about bands named Nirvana
-    results = query_by_name(ARTIST_URL, query_type["simple"], "Nirvana")
+    results = query_by_name(ARTIST_URL, query_type["simple"], "First Aid Kit")
     pretty_print(results)
 
     # Isolate information from the 4th band returned (index 3)
@@ -91,4 +96,64 @@ def main():
 if __name__ == '__main__':
     main()
 
+# 1. How many bands named "First Aid Kit"?
+    # results['artists'][0].keys()
+    # print(results['artists'][0]["name"])
+    # len(results['artists'])
+    artistCount = 0
+    itemcount = len(results['artists'])
+    for i in range(itemcount):
+        if results['artists'][i]['name'] == "First Aid Kit":
+            print(i, results['artists'][i]['name'], 'id',results['artists'][i]['id'])
+            artistCount += 1
+
+
+    print(artistCount)
+
+# 2. Begin-Area Name for Queen?
+    # print(results["artists"][0]["begin-area"]["name"])
+i=0
+for i in range(itemcount):
+    print(results['artists'][i]['name'], results['artists'][i]['type'])
+    if (results['artists'][i]['name'] == "Queen") and (results['artists'][i]['type'] == "Group"):
+        print(i, results['artists'][i]['begin-area']['name'])
+        break
+
+# 3. Spanish Alias for Beatles?
+i=0
+for i in range(itemcount):
+    print(results['artists'][i]['name'], results['artists'][i]['type'])
+    if (results['artists'][i]['name'] == "The Beatles") and (results['artists'][i]['type'] == "Group"):
+        print(i)
+        break
+
+numofAliases=len(results['artists'][i]['aliases'])
+j=0
+for j in range(numofAliases):
+    if (results['artists'][i]['aliases'][j]['locale'] == "es"):
+        print(i, j, "es")
+        results['artists'][i]['aliases'][j]['name']
+        break
     
+# 4. Nirvana disambiguation?
+    # results["artists"][0]["disambiguation"]
+    # >>> '90s US grunge band'
+i=0
+for i in range(itemcount):
+    print(results['artists'][i]['name'], results['artists'][i]['type'])
+    if (results['artists'][i]['name'] == "Nirvana") and (results['artists'][i]['type'] == "Group"):
+        print(json.dumps(results["artists"][i]["disambiguation"]))
+        break
+
+# 5. When was one direction formed?
+# # Q4 
+i=0
+for i in range(itemcount):
+    print(results['artists'][i]['name'], results['artists'][i]['type'])
+    if (results['artists'][i]['name'] == "One Direction") and (results['artists'][i]['type'] == "Group"):
+        print(json.dumps(results["artists"][i]["life-span"]["begin"]))
+        break
+"2010-07-23"
+
+
+
